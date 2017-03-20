4278368206396610b3cf3ac71a8f87d81e1fb236
KrisYu <abcyuxue123@126.com>
2017-03-19 21:10:09 -0500

    update for swift 3.0 and tested on my device

--- LivePhotoToAnimatedGif/ViewController.swift
+++ LivePhotoToAnimatedGif/ViewController.swift
@@ -30,54 +30,56 @@ class ViewController: UIViewController {
 
 	func checkAuthorization() {
 		PHPhotoLibrary.requestAuthorization { (status) -> Void in
-			if status == .Authorized {
+			if status == .authorized {
 				self.searchLivePhoto()
 			}
 		}
 	}
 
-	func searchLivePhoto() {
-		let opts = PHFetchOptions()
-		opts.predicate = NSPredicate(format: "mediaSubtype & %d != 0", PHAssetMediaSubtype.PhotoLive.rawValue)
-		let result = PHAsset.fetchAssetsWithMediaType(.Image, options: opts)
-		
-		// get live photo data
-		if let photoAsset = result.lastObject as? PHAsset {
-			PHImageManager.defaultManager().requestLivePhotoForAsset(photoAsset, targetSize: CGSizeZero, contentMode: .Default, options: nil) { (livePhoto, info) -> Void in
-				if let livePhoto = livePhoto {
-					self.livePhotoView.livePhoto = livePhoto
-				}
-			}
-		}
-	}
-	
-	@IBAction func convert(sender: AnyObject) {
+    
+    func searchLivePhoto() {
+        let opts = PHFetchOptions()
+        opts.predicate = NSPredicate(format: "mediaSubtype & %d != 0", PHAssetMediaSubtype.photoLive.rawValue)
+        let result = PHAsset.fetchAssets(with: .image, options: opts)
+        // get live photo data
+        if let photoAsset = result.lastObject as PHAsset! {
+            PHImageManager.default().requestLivePhoto(for: photoAsset, targetSize: CGSize.zero, contentMode: .aspectFit, options: nil, resultHandler: { (livePhoto, _) in
+                if let livePhoto = livePhoto{
+                    self.livePhotoView.livePhoto = livePhoto                }
+            })
+        }
+        
+        
+    }
+    
+    
+	@IBAction func convert(_ sender: AnyObject) {
 		guard let livePhoto = self.livePhotoView.livePhoto else {
 			return
 		}
 		
 		// search movie in live photo
-		let resources = PHAssetResource.assetResourcesForLivePhoto(livePhoto)
+		let resources = PHAssetResource.assetResources(for: livePhoto)
 		for resource in resources {
-			if resource.type == .PairedVideo {
+			if resource.type == .pairedVideo {
 				self.getMovieData(resource)
 				break
 			}
 		}
 	}
 	
-	func getMovieData(resource: PHAssetResource) {
-		let moviePath = (NSTemporaryDirectory() as NSString).stringByAppendingPathComponent(resource.originalFilename)
-		let movieURL = NSURL(fileURLWithPath: moviePath)
+	func getMovieData(_ resource: PHAssetResource) {
+		let moviePath = (NSTemporaryDirectory() as NSString).appendingPathComponent(resource.originalFilename)
+		let movieURL = URL(fileURLWithPath: moviePath)
 		let movieData = NSMutableData()
 
 		// load movie data
-		PHAssetResourceManager.defaultManager().requestDataForAssetResource(resource, options: nil, dataReceivedHandler: { (data) -> Void in
-			movieData.appendData(data)
+		PHAssetResourceManager.default().requestData(for: resource, options: nil, dataReceivedHandler: { (data) -> Void in
+			movieData.append(data)
 		}) { (err) -> Void in
 			do {
-				try movieData.writeToURL(movieURL, options: NSDataWritingOptions.AtomicWrite)
-				let movieAsset = AVAsset(URL: movieURL)
+				try movieData.write(to: movieURL, options: NSData.WritingOptions.atomicWrite)
+				let movieAsset = AVAsset(url: movieURL)
 				self.convertToGif(movieAsset, resource: resource)
 			} catch {
 				
@@ -85,7 +87,7 @@ class ViewController: UIViewController {
 		}
 	}
 	
-	func convertToGif(movieAsset: AVAsset, resource: PHAssetResource) {
+	func convertToGif(_ movieAsset: AVAsset, resource: PHAssetResource) {
 		// gif frames
 		let numFrame = 30
 		let frameValue = movieAsset.duration.value / Int64(numFrame)
@@ -93,16 +95,16 @@ class ViewController: UIViewController {
 		var times = Array<NSValue>()
 		for i in 0..<numFrame {
 			let time = CMTimeMakeWithEpoch(frameValue * Int64(i), timeScale, movieAsset.duration.epoch)
-			times.append(NSValue(CMTime: time))
+			times.append(NSValue(time: time))
 		}
 		
-		guard let docDir = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true).first else {
+		guard let docDir = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first else {
 			return
 		}
-		let gifPath = (docDir as NSString).stringByAppendingPathComponent((resource.originalFilename as NSString).stringByDeletingPathExtension + ".gif")
-		let gifURL = NSURL(fileURLWithPath: gifPath)
+		let gifPath = (docDir as NSString).appendingPathComponent((resource.originalFilename as NSString).deletingPathExtension + ".gif")
+		let gifURL = URL(fileURLWithPath: gifPath)
 
-		guard let gif = CGImageDestinationCreateWithURL(gifURL, kUTTypeGIF, numFrame, nil) else {
+		guard let gif = CGImageDestinationCreateWithURL(gifURL as CFURL, kUTTypeGIF, numFrame, nil) else {
 			return
 		}
 		
@@ -117,19 +119,19 @@ class ViewController: UIViewController {
 		generator.requestedTimeToleranceAfter = kCMTimeZero
 		// apply video transform
 		generator.appliesPreferredTrackTransform = true
-		generator.maximumSize = CGSizeMake(640, 640)
-		generator.generateCGImagesAsynchronouslyForTimes(times) { (requested, image, actual, result, err) -> Void in
+		generator.maximumSize = CGSize(width: 640, height: 640)
+		generator.generateCGImagesAsynchronously(forTimes: times) { (requested, image, actual, result, err) -> Void in
 			if let image = image {
-				CGImageDestinationAddImage(gif, image, frameProperty)
+				CGImageDestinationAddImage(gif, image, frameProperty as CFDictionary?)
 			}
 			cnt += 1
 			if cnt >= numFrame {
 				let gifProperty = [String(kCGImagePropertyGIFDictionary): [String(kCGImagePropertyGIFLoopCount): 0]]
-				CGImageDestinationSetProperties(gif, gifProperty)
+				CGImageDestinationSetProperties(gif, gifProperty as CFDictionary?)
 				CGImageDestinationFinalize(gif)
 				
-				dispatch_async(dispatch_get_main_queue(), { () -> Void in
-					self.webView.loadRequest(NSURLRequest(URL: gifURL))
+				DispatchQueue.main.async(execute: { () -> Void in
+					self.webView.loadRequest(URLRequest(url: gifURL))
 				})
 			}
 		}
